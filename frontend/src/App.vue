c2a63a0cf14c08302cba246acb17721e3aee2fed
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-06-26 19:33:28 +0200

    Create App.vue

---
+++ frontend/src/App.vue
@@ -0,0 +1,16 @@
+<script>
+import { defineComponent } from "vue";
+import NavBar from "@/components/NavBar.vue";
+
+export default defineComponent({
+  components: { NavBar }
+});
+</script>
+
+<template>
+  <NavBar></NavBar>
+  <RouterView></RouterView>
+</template>
+
+<style scoped>
+</style>
