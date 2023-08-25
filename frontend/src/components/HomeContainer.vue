f1ea09895e27fb6fac97a46a80a7b22713ce833a
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-08-25 12:09:37 +0200

    Update HomeContainer.vue

--- frontend/src/components/HomeContainer.vue
+++ frontend/src/components/HomeContainer.vue
@@ -14,39 +14,11 @@ export default defineComponent({
 
 <template>
     <div class="button-container">
-      <router-link class="button" :to="{path: '/'}">Nuovo Ticket</router-link>
-      <router-link class="button" :to="{path: '/'}">Elenco Ticket</router-link>
+      <router-link class="btn btn-warning" :to="{path: '/'}">Nuovo Ticket</router-link>
+      <router-link class="btn btn-warning" :to="{path: '/'}">Elenco Ticket</router-link>
     </div>
 </template>
 
 <style scoped>
 @import url('../../templates/style.css');
-
-a {
-  text-decoration: none;
-}
-
-.button-container {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-}
-
-.button {
-  margin: 1rem;
-  padding: 0.5rem 1rem;
-  font-size: 1rem;
-  border: none;
-  border-radius: 4px;
-  background-color: #007bff;
-  color: white;
-  cursor: pointer;
-  transition: background-color 0.2s ease-in-out;
-}
-
-.button:hover {
-  background-color: #0069d9;
-}
 </style>
