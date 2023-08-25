564b0587327902e26e17696a2a5ee7a67f82bf30
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-08-25 18:51:41 +0200

    Add modal for adding ticket

--- frontend/src/components/home/HomeContainer.vue
+++ frontend/src/components/home/HomeContainer.vue
@@ -2,14 +2,39 @@
 import {defineComponent} from "vue";
 
 export default defineComponent({
-  name: "HomeContainer"
+  name: "HomeContainer",
+  data() {
+    return {
+      showModal: false
+    };
+  }
 })
 </script>
 
 <template>
     <div class="btn-container">
-      <router-link class="btn btn-warning btn-with-margin" :to="{path: '/'}">Nuovo Ticket</router-link>
+      <button class="btn btn-warning btn-with-margin" data-bs-toggle="modal" data-bs-target="#modalExample">Nuovo Ticket</button>
       <router-link class="btn btn-warning btn-with-margin" :to="{path: '/tickets'}">Elenco Ticket</router-link>
+
+      <div class="modal" id="modalExample">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">Modal title</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"></span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <p>Modal body text goes here.</p>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-primary">Save changes</button>
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
 </template>
 
