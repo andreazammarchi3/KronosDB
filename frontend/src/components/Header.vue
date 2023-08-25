ad35a28b88a13fdda425e2eccc4b97580b8465fb
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-08-25 13:47:29 +0200

    Update Header.vue

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,16 +1,20 @@
+<script>
+
+</script>
+
 <template>
   <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
     <div class="container-fluid">
-      <a class="navbar-brand" href="#">KronosDB</a>
+      <router-link class="navbar-brand" :to="{path: '/'}">KronosDB</router-link>
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarColor02">
         <ul class="navbar-nav me-auto">
           <li class="nav-item">
-            <a class="nav-link active" href="#">Home
+            <router-link class="nav-link active" :to="{path: '/'}">Home
               <span class="visually-hidden">(current)</span>
-            </a>
+            </router-link>
           </li>
         </ul>
       </div>
