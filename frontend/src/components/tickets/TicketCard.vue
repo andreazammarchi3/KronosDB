2dbe6f1f78817290c071fe3e3680e4b67862ce67
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-08-25 14:48:33 +0200

    Update TicketCard.vue

--- frontend/src/components/tickets/TicketCard.vue
+++ frontend/src/components/tickets/TicketCard.vue
@@ -1,13 +1,28 @@
 <script>
 import {defineComponent} from "vue";
+import {BASE_URL} from "@/main";
+import axios from "axios";
 
 export default defineComponent({
   name: "TicketCard",
   props: ['ticket'],
   data() {
     return {
+      clientFullName: null,
       routeName: "TicketDetails"
     }
+  },
+  methods: {
+    getClientFullName() {
+      axios.get(`${BASE_URL}/getClient:${this.ticket.idClient}`).then(response => {
+        this.clientFullName = response.data
+      }).catch(error => {
+        console.log(error)
+      })
+    }
+  },
+  mounted() {
+    this.getClientFullName()
   }
 })
 </script>
@@ -16,7 +31,7 @@ export default defineComponent({
   <div class="card bg-secondary mb-3" style="max-width: 20rem;">
     <div class="card-header">{{ this.ticket.idTicket }}</div>
     <div class="card-body">
-      <h4 class="card-title">{{ this.ticket.idClient }}</h4>
+      <h4 class="card-title">{{ this.clientFullName.fullName }}</h4>
       <p class="card-text">{{ this.ticket.clientRequest }}</p>
     </div>
   </div>
