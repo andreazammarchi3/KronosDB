3480281d6743a1836edb48b76ad7e4f62a49bdfb
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-08-25 11:59:21 +0200

    Create Header.vue

---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,23 @@
+<template>
+  <header class="header">
+    <div class="header__logo">KronosDB</div>
+  </header>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  background: linear-gradient(to right, #007bff, #0062cc);
+  color: white;
+  padding: 1rem;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+}
+
+.header__logo {
+  font-size: 1.5rem;
+  font-weight: bold;
+  text-shadow: 1px 1px 2px rgba(0, 0, 0, 0.2);
+}
+</style>
