ef6b524e455ae9d3afa4cf7ba478d0780aab1fcc
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-08-26 14:37:41 +0200

    Update TicketsPage.vue

--- frontend/src/pages/TicketsPage.vue
+++ frontend/src/pages/TicketsPage.vue
@@ -5,7 +5,7 @@ import Header from "@/components/Header.vue";
 import TicketsList from "@/components/tickets/TicketsList.vue";
 
 export default defineComponent({
-  name: "HomePage",
+  name: "TicketPage",
   components: {TicketsList, Header}
 })
 </script>
