70e6a9967f45834ffc38187a838bce5949196f1e
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-09-01 18:02:01 +0200

    Update chatsModel.js

--- backend/src/models/chatsModel.js
+++ backend/src/models/chatsModel.js
@@ -2,7 +2,12 @@ const mongoose = require('mongoose')
 
 const chatSchema = new mongoose.Schema({
     topic: Number,
-    messages: [],
+    messages: [{
+        id: Number,
+        senderId: Number,
+        time: String,
+        text: String,
+    }],
 });
 
 module.exports = mongoose.model('chats', chatSchema);
