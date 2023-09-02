f30ff2bc952bc2940fcdb0d33352cbab08d1a24d
Andrea Zammarchi <andrea.zammarchi3@studio.unibo.it>
2023-09-02 16:22:05 +0200

    Update Notification.vue

--- frontend/src/components/Notification.vue
+++ frontend/src/components/Notification.vue
@@ -13,9 +13,26 @@ export default defineComponent({
 </script>
 
 <template>
-  <div v-if="numberOfNotifications > 0"> {{ this.numberOfNotifications }} {{ this.numberOfNotifications === 1 ? 'nuovo messaggio' : 'nuovi messaggi' }} su: {{ topic }}</div>
+  <div v-if="numberOfNotifications > 0" class="notification-container">
+    <i class="bi bi-x"></i>
+    <div class="notification-text"> {{ this.numberOfNotifications }} {{ this.numberOfNotifications === 1 ? 'nuovo messaggio' : 'nuovi messaggi' }} su: {{ topic }}</div>
+  </div>
 </template>
 
 <style scoped>
+.notification-container {
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+}
 
+.notification-container .bi-x {
+  font-size: 30px;
+  line-height: 0.6;
+}
+
+.notification-container .notification-text {
+  margin-left: 8px;
+  margin-top: 2px;
+}
 </style>
